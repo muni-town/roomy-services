05bfe4163d910f6d63a0163295f3d68adcf21b7a
Zicklag <zicklag@katharostech.com>
2025-04-23 16:57:17 -0500

    refactor: update syncserver to use socket.io instead of websockets.

--- roomy-syncserver/main.ts
+++ roomy-syncserver/main.ts
@@ -1,18 +1,49 @@
-import { AutoRouter, cors, error } from "itty-router";
 import { IdResolver } from "@atproto/identity";
 import { verifyJwt } from "@atproto/xrpc-server";
 import encodeBase32 from "base32-encode";
-import { SuperPeer1, StorageManager, SuperPeer1BinaryWrapper } from "@muni-town/leaf";
+import { SuperPeer1, StorageManager } from "@muni-town/leaf";
 import { denoKvBlobStorageAdapter } from "@muni-town/leaf-storage-deno-kv";
+import { attachServer, io } from "@muni-town/leaf-sync-socket-io-server";
+import { createServer } from "node:http";
+import express from "express";
 
 // Parse configuration environment variables.
 const dataDir = Deno.env.get("DATA_DIR");
 const serviceDid = Deno.env.get("DID");
 const serviceEndpoint = Deno.env.get("PUBLIC_URL");
+if (!serviceEndpoint) {
+  console.error("PUBLIC_URL environment variable is required.");
+  Deno.exit(1);
+}
 const unsafeDevToken = Deno.env.get("UNSAFE_DEV_TOKEN");
+const port = parseInt(Deno.env.get("PORT") || "8000");
 
+// Setup the key-value store, super peer, and socket.io server.
 const kv = await Deno.openKv(dataDir);
-const superPeer = new SuperPeer1(new StorageManager(denoKvBlobStorageAdapter(kv)));
+const superPeer = new SuperPeer1(
+  new StorageManager(denoKvBlobStorageAdapter(kv))
+);
+const socketIoServer = new io.Server({
+  cors: {
+    methods: "*",
+    origin: "*",
+  },
+});
+/** Authenticate incoming socket.io connections. */
+socketIoServer.use(async (socket, next) => {
+  const { did, token } = socket.handshake.auth;
+
+  if (typeof did !== "string" || typeof token !== "string")
+    return next(new Error("`did` and `token` required in Socket.io auth"));
+
+  // Load the token and make sure the DID matches to make sure it's valid.
+  const tokenDid = (await kv.get<string>(["tokens", token])).value;
+
+  if (did !== tokenDid && !(unsafeDevToken && token === unsafeDevToken))
+    return new Error("Token invalid or expired");
+
+  next();
+});
 
 // TODO: add a DID cache using Deno KV
 const idResolver = new IdResolver();
@@ -27,11 +58,7 @@ async function getSigningKey(
   return atprotoData.signingKey;
 }
 
-const { preflight, corsify } = cors();
-const router = AutoRouter({
-  before: [preflight],
-  finally: [corsify],
-});
+const app = express();
 
 if (!serviceDid)
   throw new Error(
@@ -39,71 +66,18 @@ if (!serviceDid)
   );
 
 // Return the service DID
-router.get("/.well-known/did.json", ({ url }) => ({
-  "@context": ["https://www.w3.org/ns/did/v1"],
-  id: serviceDid,
-  service: [
-    {
-      id: "#roomy_syncserver",
-      type: "RoomySyncserver",
-      serviceEndpoint: (() => {
-        if (serviceEndpoint) return serviceEndpoint;
-        const u = new URL(url);
-        u.pathname = "/";
-        return u.href;
-      })(),
-    },
-  ],
-}));
-
-// Open a websocket connection to the routing service.
-router.get("/sync/as/:did", async (req) => {
-  const { headers, params } = req;
-
-  // Get that the user is trying to connect as from the URL.
-  const did = params.did!;
-
-  // Make sure this is a websocket request
-  if (headers.get("upgrade") != "websocket") {
-    return error(400, "Must set `upgrade` header to `websocket`.");
-  }
-
-  // Get the authorization token from the header
-  const token = headers
-    .get("Sec-WebSocket-Protocol")
-    ?.split("authorization,")[1]
-    ?.trim();
-  if (!token) return error(403, "Missing authorization bearer token");
-
-  // Load the token and make sure the DID matches to make sure it's valid.
-  const tokenDid = (await kv.get<string>(["tokens", token])).value;
-
-  if (did !== tokenDid && !(unsafeDevToken && token === unsafeDevToken))
-    return error(403, "Token invalid or expired");
-
-  // Upgrade to websocket connection
-  const { socket, response } = Deno.upgradeWebSocket(req, {
-    protocol: "authorization",
+app.get("/.well-known/did.json", (_req, res) => {
+  res.send({
+    "@context": ["https://www.w3.org/ns/did/v1"],
+    id: serviceDid,
+    service: [
+      {
+        id: "#roomy_syncserver",
+        type: "RoomySyncserver",
+        serviceEndpoint,
+      },
+    ],
   });
-  socket.binaryType = "arraybuffer";
-
-  const backend = new SuperPeer1BinaryWrapper(superPeer);
-
-  socket.addEventListener("open", () => {
-    backend.setReceiver((data) => {
-      socket.send(data);
-    });
-    socket.addEventListener("message", (ev) => {
-      if (ev.data instanceof ArrayBuffer) {
-        backend.send(new Uint8Array(ev.data));
-      }
-    });
-    socket.addEventListener("close", () => {
-      backend.cleanup();
-    });
-  });
-
-  return response;
 });
 
 //
@@ -117,17 +91,25 @@ type AuthCtx = {
   jwtPayload: JwtPayload;
   did: string;
 };
-type Ctx = Request & AuthCtx;
+// type Req =
 
-router.all("*", async (ctx) => {
-  const url = new URL(ctx.url);
-  if (!url.pathname.startsWith("/xrpc/")) return error(404);
-  const lxm = url.pathname.split("/xrpc/")[1];
+app.use(async (req, res, next) => {
+  if (!req.url.startsWith("/xrpc/")) {
+    res.status(404).send("Page not found");
+    return;
+  }
+  const lxm = req.url.split("/xrpc/")[1];
 
-  const authorization = ctx.headers.get("authorization");
-  if (!authorization) return error(403, "Authorization token required.");
-  if (!authorization.startsWith("Bearer "))
-    return error(403, "Bearer token required");
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(403).send("Authorization token required.");
+    return;
+  }
+  if (!authorization.startsWith("Bearer ")) {
+    res.status(403).send("Bearer token required");
+    return;
+  }
+
   const jwt = authorization.split("Bearer ")[1];
   let jwtPayload: JwtPayload;
   try {
@@ -135,17 +117,18 @@ router.all("*", async (ctx) => {
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error("Error validating JWT:", e);
-    return error(403, "Could not validate authorization JWT.");
+    res.status(403).send("Could not validate authorization JWT.");
+    return;
   }
 
-  ctx.jwtPayload = jwtPayload;
-  ctx.did = jwtPayload.iss;
+  (req as unknown as AuthCtx).jwtPayload = jwtPayload;
+  (req as unknown as AuthCtx).did = jwtPayload.iss;
 
-  return undefined;
+  next();
 });
 
 // Get an access token that can be used to open up a WebSocket connection to the router.
-router.get("/xrpc/chat.roomy.v0.sync.token", async ({ did }: Ctx) => {
+app.get("/xrpc/chat.roomy.v0.sync.token", async (req, res) => {
   // Generate a new token
   const token = encodeBase32(
     crypto.getRandomValues(new Uint8Array(32)),
@@ -153,13 +136,17 @@ router.get("/xrpc/chat.roomy.v0.sync.token", async ({ did }: Ctx) => {
   );
   // Add the token to the key-value store and give it a lifetime of 30 seconds. Login attempts after
   // that time will fail.
-  await kv.set(["tokens", token], did, {
+  await kv.set(["tokens", token], (req as unknown as AuthCtx).did, {
     expireIn: 30000,
   });
 
-  return {
+  res.send({
     token,
-  };
+  });
 });
 
-Deno.serve(router.fetch);
+// Add the router as middleware to the socket.io server
+const httpServer = createServer(app);
+attachServer(superPeer, socketIoServer, httpServer);
+httpServer.listen(port);
+console.log(`Listening on port ${port}`);
