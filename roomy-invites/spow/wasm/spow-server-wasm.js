33f7a0fa96079f33e03a4744590c0bf1459077ae
Zicklag <zicklag@katharostech.com>
2025-07-12 10:45:26 -0500

    fix: try to fix deno deploy WASM loading.

--- roomy-invites/spow/wasm/spow-server-wasm.js
+++ roomy-invites/spow/wasm/spow-server-wasm.js
@@ -1,6 +1,6 @@
 export * from "./spow-server-wasm_bg.js";
 import * as bg from "./spow-server-wasm_bg.js"
-const wasmBytes = await Deno.readFile("./spow/wasm/spow-server-wasm_bg.wasm");
+const wasmBytes = await Deno.readFile("roomy-invites/spow/wasm/spow-server-wasm_bg.wasm");
 const wasm = (await WebAssembly.instantiate(wasmBytes, {
   "./spow-server-wasm_bg.js": bg,
 })).instance.exports;
