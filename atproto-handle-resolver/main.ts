17ea98f1da80bf277dacf222cdea312c00a3de42
Zicklag <zicklag@katharostech.com>
2026-01-21 16:49:36 -0600

    feat: add endpoint to resolve leaf handles.

--- atproto-handle-resolver/main.ts
+++ atproto-handle-resolver/main.ts
@@ -1,7 +1,7 @@
 import { AutoRouter, cors, error } from "itty-router";
 import { AtprotoHandleResolver } from "@atproto-labs/handle-resolver";
 
-const resolver = new AtprotoHandleResolver({
+const atprotoResolver = new AtprotoHandleResolver({
   async resolveTxt(hostname) {
     try {
       return (await Deno.resolveDns(hostname, "TXT")).flat();
@@ -22,8 +22,29 @@ router.get("/xrpc/com.atproto.identity.resolveHandle", async ({ query }) => {
   const { handle } = query;
   if (typeof handle !== "string" || !handle)
     return error(400, "handle query parameter required");
-  const did: string | null = await resolver.resolve(handle);
+  const did: string | null = await atprotoResolver.resolve(handle);
   if (!did) return error(404, `Could not resolve handle to DID: ${handle}`);
+  return { did };
+});
+
+router.get("/xrpc/town.muni.leaf.resolveHandle", async ({ query }) => {
+  const { handle } = query;
+  if (typeof handle !== "string" || !handle)
+    return error(400, "handle query parameter required");
+
+  const txtRecords = (await Deno.resolveDns(`_leaf.${handle}`, "TXT"))
+    .flat()
+    .flatMap((x) => {
+      const split = x.split("=");
+      if (split.length == 2) {
+        const [key, value] = split;
+        return [{ key, value }];
+      }
+      return [];
+    });
+  const did: string | undefined = txtRecords.find((x) => x.key == "did")?.value;
+  if (!did || !did.startsWith("did:"))
+    return error(404, `Could not resolve handle to DID: ${handle}`);
   return { did };
 });
 
